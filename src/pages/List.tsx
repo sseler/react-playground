ad94e1961bee183163148b5e12bd17f8b412c10e
Filip Jaskulski <filip.jaskulski@adtonos.com>
2023-12-18 17:15:57 +0100

    init

---
+++ src/pages/List.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react"
+import { ListData } from "../static/ListData"
+
+/*
+1. Show every item from the ListData;
+2. Create filtering option by type of product
+3. Create search bar to searching by word. 
+*/
+const List:FC = () => {
+    return (
+        <>
+        <ul>
+
+        </ul>
+        </>
+    )
+}
---
+++ src/static/ListData.ts
@@ -0,0 +1,24 @@
+enum ProductType  {
+    "FRUIT" =  "fruit",
+    "VEG" = 'vegetable',
+    "MEAT" = "meat",
+    "DRINK" = 'drinks',
+}
+
+
+export const ListData = [
+    {id: 101, name: 'Banana', type: ProductType.FRUIT},
+    {id: 102, name: 'Papric', type: ProductType.VEG},
+    {id: 103, name: 'Water', type: ProductType.DRINK},
+    {id: 104, name: 'Carrot', type: ProductType.VEG},
+    {id: 105, name: 'Watermelon', type: ProductType.FRUIT},
+    {id: 106, name: 'Beef', type: ProductType.MEAT},
+    {id: 107, name: 'Lettuce', type: ProductType.VEG},
+    {id: 108, name: 'Zuccini', type: ProductType.VEG},
+    {id: 109, name: 'Mango', type: ProductType.FRUIT},
+    {id: 110, name: 'Chicken', type: ProductType.MEAT},
+    {id: 111, name: 'Milk', type: ProductType.DRINK},
+    {id: 112, name: 'Pineapple', type: ProductType.FRUIT},
+    {id: 113, name: 'Coke', type: ProductType.DRINK},
+
+]
