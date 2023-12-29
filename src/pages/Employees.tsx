4e1660537baaaf9a5433e80bb095018d127b2e57
Filip Jaskulski <filip.jaskulski@adtonos.com>
2023-12-29 11:28:48 +0100

    new task

---
+++ src/pages/Employees.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react"
+
+
+/*
+*** CORE:
+1. Fetch all employees @url =  https://dummy.restapiexample.com/api/v1/employees
+2. Make html table component and render them properly
+3. Create preview of one employee @url = https://dummy.restapiexample.com/api/v1/employee/${id}
+4. Create option to edit the employee @url = https://dummy.restapiexample.com/api/v1/update/${id}
+5. Create option to delete the employee @url = https://dummy.restapiexample.com/api/v1/delete/${id}
+
+*** EXTRA TASK: 
+1. Create simple backend application using express or fastify framework and mongodb database.
+2. Handle all requests from CORE section to get rid off fake API
+3. Create the way to upload image from browser and save in on the backend (no need to save it in the db, simple file saving would be ok)
+*/
+export const Employees:FC = () => {
+    return (
+    <div></div>    
+    )
+}
