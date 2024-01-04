e633b346c1e2cf777ed2062220f23cc659690c16
Filip Jaskulski <filip.jaskulski@adtonos.com>
2024-01-04 17:11:36 +0100

    commit

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -5,13 +5,26 @@ import { ListData } from "../static/ListData"
 1. Show every item from the ListData;
 2. Create filtering option by type of product
 3. Create search bar to searching by word. 
+
+const handleSearch = (event) => {
+
+    if (event.target.value === "") {
+      setFilterList(list);
+      return;
+    }
+    const filteredValues = list.filter(
+      (item) =>
+        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues);
+  };
 */
-const List:FC = () => {
+export const List:FC = () => {
     return (
         <>
-        <ul>
+          <ul>
 
-        </ul>
+          </ul>
         </>
     )
 }
---
+++ src/pages/Products.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react"
+
+
+/*
+*** CORE:
+1. Fetch all products
+2. Make html table component and render them properly
+3. Create preview of one product
+4. Create option to edit the product 
+5. Create option to delete the product 
+@URLS => https://dummyjson.com/docs/products
+
+*** EXTRA TASK: 
+1. Create simple backend application using express or fastify framework and mongodb database.
+2. Handle all requests from CORE section to get rid off fake API
+3. Create the way to upload product image from browser and save in on the backend (no need to save it in the db, simple file saving would be ok)
+*/
+export const Products:FC = () => {
+    return (
+    <div></div>    
+    )
+}
